dc6fbe74a32450fbb94f9ebd6a023a5860615f3d
edukrause <129079233+edukrause@users.noreply.github.com>
2025-08-26 17:38:01 -0300

    Pasta correta

--- CalculadoraBDD/teste/Script/testesScripts.js
+++ CalculadoraBDD/teste/Script/testesScripts.js
@@ -1,159 +1,278 @@
 ﻿function somar() {
   TestedApps.calc.Run();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.Teclado_num_rico.Cinco.Click();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.Operadores_padr_o.Mais.Click();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.Teclado_num_rico.Tr_s.Click();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.Operadores_padr_o.Igual_a.Click();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.A_exibi_o_0.TextContainer.NormalOutput.Click(14, 41);
-  aqObject.CheckProperty(Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.A_exibi_o_8.TextContainer.NormalOutput, "Text", cmpEqual, "8");
+  Delay(2000);
+  
+  Sys.Keys("5");
+  Sys.Keys("+");
+  Sys.Keys("3");
+  Sys.Keys("[Enter]");
+  
+  Delay(500);
+  
+  // Encontra o display com o resultado "8" de forma genérica
+  var displayArea = Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget;
+  var resultDisplay = displayArea.FindChild("Text", "8", 10);
+  
+  // Verificação tradicional que funciona em qualquer máquina
+  aqObject.CheckProperty(resultDisplay, "Text", cmpEqual, "8");
+  
   Aliases.ApplicationFrameHost.wndCalculadora.Close();
 }
 
 function subtrair() {
   TestedApps.calc.Run();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.Teclado_num_rico.Nove.Click();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.Operadores_padr_o.Menos.Click();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.Teclado_num_rico.Quatro.Click();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.Operadores_padr_o.Igual_a.Click();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.A_exibi_o_0.TextContainer.NormalOutput.Click(9, 29);
-  aqObject.CheckProperty(Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.A_exibi_o_5.TextContainer.NormalOutput, "Text", cmpEqual, "5");
+  Delay(2000);
+  
+  Sys.Keys("9");
+  Sys.Keys("-");
+  Sys.Keys("4");
+  Sys.Keys("[Enter]");
+  
+  Delay(500);
+  
+  // Encontra o display com o resultado "5" de forma genérica
+  var displayArea = Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget;
+  var resultDisplay = displayArea.FindChild("Text", "5", 10);
+  
+  aqObject.CheckProperty(resultDisplay, "Text", cmpEqual, "5");
+  
   Aliases.ApplicationFrameHost.wndCalculadora.Close();
 }
 
 function multiplicar() {
   TestedApps.calc.Run();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.Teclado_num_rico.Sete.Click();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.Operadores_padr_o.Multiplicar_por.Click();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.Teclado_num_rico.Seis.Click();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.Operadores_padr_o.Igual_a.Click();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.A_exibi_o_0.TextContainer.NormalOutput.Click(25, 43);
-  aqObject.CheckProperty(Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.A_exibi_o_42.TextContainer.NormalOutput, "Text", cmpEqual, "42");
+  Delay(2000);
+  
+  Sys.Keys("7");
+  Sys.Keys("*");
+  Sys.Keys("6");
+  Sys.Keys("[Enter]");
+  
+  Delay(500);
+  
+  // Encontra o display com o resultado "42" de forma genérica
+  var displayArea = Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget;
+  var resultDisplay = displayArea.FindChild("Text", "42", 10);
+  
+  aqObject.CheckProperty(resultDisplay, "Text", cmpEqual, "42");
+  
   Aliases.ApplicationFrameHost.wndCalculadora.Close();
 }
 
 function dividir() {
   TestedApps.calc.Run();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.Teclado_num_rico.Oito.Click();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.Operadores_padr_o.Dividir_por.Click();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.Teclado_num_rico.Dois.Click();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.Operadores_padr_o.Igual_a.Click();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.A_exibi_o_0.TextContainer.NormalOutput.Click(12, 22);
-  aqObject.CheckProperty(Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.A_exibi_o_4.TextContainer.NormalOutput, "Text", cmpEqual, "4");
+  Delay(2000);
+  
+  Sys.Keys("8");
+  Sys.Keys("/");
+  Sys.Keys("2");
+  Sys.Keys("[Enter]");
+  
+  Delay(500);
+  
+  // Encontra o display com o resultado "4" de forma genérica
+  var displayArea = Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget;
+  var resultDisplay = displayArea.FindChild("Text", "4", 10);
+  
+  aqObject.CheckProperty(resultDisplay, "Text", cmpEqual, "4");
+  
   Aliases.ApplicationFrameHost.wndCalculadora.Close();
 }
 
 function dividirPorZero() {
   TestedApps.calc.Run();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.Teclado_num_rico.Nove.Click();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.Operadores_padr_o.Dividir_por.Click();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.Teclado_num_rico.Zero.Click();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.Operadores_padr_o.Igual_a.Click();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.A_exibi_o_0.N_o_poss_vel_dividir_por_zero.N_o_poss_vel_dividir_por_zero.Click(212, 22);
-  aqObject.CheckProperty(Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.A_exibi_o_N_o_poss_vel_dividir_por_zero.N_o_poss_vel_dividir_por_zero.N_o_poss_vel_dividir_por_zero, "Text", cmpEqual, "Não é possível dividir por zero");
+  Delay(2000);
+  
+  Sys.Keys("9");
+  Sys.Keys("/");
+  Sys.Keys("0");
+  Sys.Keys("[Enter]");
+  
+  Delay(500);
+  
+  // Encontra o display com a mensagem de erro de forma genérica
+  var displayArea = Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget;
+  var resultDisplay = displayArea.FindChild("Text", "Não é possível dividir por zero", 10);
+  
+  aqObject.CheckProperty(resultDisplay, "Text", cmpEqual, "Não é possível dividir por zero");
+  
   Aliases.ApplicationFrameHost.wndCalculadora.Close();
 }
 
 function botaoCE() {
   TestedApps.calc.Run();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.Teclado_num_rico.Cinco.Click();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.Operadores_padr_o.Mais.Click();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.Teclado_num_rico.Tr_s.Click();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.Controles_de_exibi_o.Limpar_entrada.Click();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.A_exibi_o_0.TextContainer.NormalOutput.Click(17, 33);
-  aqObject.CheckProperty(Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.A_exibi_o_0.TextContainer.NormalOutput, "Text", cmpEqual, "0");
+  Delay(2000);
+  
+  Sys.Keys("5");
+  Sys.Keys("+");
+  Sys.Keys("3");
+  Sys.Keys("[Del]"); // Tecla CE (Clear Entry)
+  
+  Delay(500);
+  
+  // Encontra o display com o resultado "0" de forma genérica
+  var displayArea = Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget;
+  var resultDisplay = displayArea.FindChild("Text", "0", 10);
+  
+  aqObject.CheckProperty(resultDisplay, "Text", cmpEqual, "0");
+  
   Aliases.ApplicationFrameHost.wndCalculadora.Close();
 }
 
 function botaoC() {
   TestedApps.calc.Run();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.Teclado_num_rico.Oito.Click();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.Operadores_padr_o.Multiplicar_por.Click();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.Teclado_num_rico.Cinco.Click();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.Operadores_padr_o.Igual_a.Click();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.A_exibi_o_0.TextContainer.NormalOutput.Click(37, 40);
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.Operadores_padr_o.Mais.Click();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.Teclado_num_rico.Dois.Click();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.Controles_de_exibi_o.Limpar.Click();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.A_exibi_o_0.TextContainer.NormalOutput.Click(12, 21);
-  aqObject.CheckProperty(Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.A_exibi_o_0.TextContainer.NormalOutput, "Text", cmpEqual, "0");
+  Delay(2000);
+  
+  Sys.Keys("8");
+  Sys.Keys("*");
+  Sys.Keys("5");
+  Sys.Keys("[Enter]");
+  Sys.Keys("+");
+  Sys.Keys("2");
+  Sys.Keys("[Esc]"); // Tecla C (Clear All)
+  
+  Delay(500);
+  
+  // Encontra o display com o resultado "0" de forma genérica
+  var displayArea = Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget;
+  var resultDisplay = displayArea.FindChild("Text", "0", 10);
+  
+  aqObject.CheckProperty(resultDisplay, "Text", cmpEqual, "0");
+  
   Aliases.ApplicationFrameHost.wndCalculadora.Close();
 }
 
 function botaoBackspace() {
   TestedApps.calc.Run();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.Teclado_num_rico.Um.Click();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.Teclado_num_rico.Dois.Click();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.Teclado_num_rico.Tr_s.Click();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.Controles_de_exibi_o.Backspace.Click();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.A_exibi_o_0.TextContainer.NormalOutput.Click(23, 48);
-  aqObject.CheckProperty(Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.A_exibi_o_12.TextContainer.NormalOutput, "Text", cmpEqual, "12");
+  Delay(2000);
+  
+  Sys.Keys("1");
+  Sys.Keys("2");
+  Sys.Keys("3");
+  Sys.Keys("[BS]"); // Tecla Backspace
+  
+  Delay(500);
+  
+  // Encontra o display com o resultado "12" de forma genérica
+  var displayArea = Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget;
+  var resultDisplay = displayArea.FindChild("Text", "12", 10);
+  
+  aqObject.CheckProperty(resultDisplay, "Text", cmpEqual, "12");
+  
   Aliases.ApplicationFrameHost.wndCalculadora.Close();
 }
 
 function operacaoNumeroNegativo() {
   TestedApps.calc.Run();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.Teclado_num_rico.Cinco.Click();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.Operadores_padr_o.Menos.Click();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.Teclado_num_rico.Oito.Click();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.Operadores_padr_o.Igual_a.Click();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.A_exibi_o_0.TextContainer.NormalOutput.Click(32, 40);
-  aqObject.CheckProperty(Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.A_exibi_o_3.TextContainer.NormalOutput, "Text", cmpEqual, "-3");
+  Delay(2000);
+  
+  Sys.Keys("5");
+  Sys.Keys("-");
+  Sys.Keys("8");
+  Sys.Keys("[Enter]");
+  
+  Delay(500);
+  
+  // Encontra o display com o resultado "-3" de forma genérica
+  var displayArea = Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget;
+  var resultDisplay = displayArea.FindChild("Text", "-3", 10);
+  
+  aqObject.CheckProperty(resultDisplay, "Text", cmpEqual, "-3");
+  
   Aliases.ApplicationFrameHost.wndCalculadora.Close();
 }
 
 function operacaoPorcentagem() {
   TestedApps.calc.Run();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.Teclado_num_rico.Dois.Click();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.Teclado_num_rico.Zero.DblClick();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.Operadores_padr_o.Multiplicar_por.Click();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.Teclado_num_rico.Um.Click();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.Teclado_num_rico.Zero.Click();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.Controles_de_exibi_o.Por_cento.Click();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.Operadores_padr_o.Igual_a.Click();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.A_exibi_o_0.TextContainer.NormalOutput.Click(33, 28);
-  aqObject.CheckProperty(Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.A_exibi_o_20.TextContainer.NormalOutput, "Text", cmpEqual, "20");
+  Delay(2000);
+  
+  Sys.Keys("2");
+  Sys.Keys("0");
+  Sys.Keys("0");
+  Sys.Keys("*");
+  Sys.Keys("1");
+  Sys.Keys("0");
+  Sys.Keys("%"); // Tecla % (Shift + 5)
+  Sys.Keys("[Enter]");
+  
+  Delay(500);
+  
+  // Encontra o display com o resultado "20" de forma genérica
+  var displayArea = Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget;
+  var resultDisplay = displayArea.FindChild("Text", "20", 10);
+  
+  aqObject.CheckProperty(resultDisplay, "Text", cmpEqual, "20");
+  
   Aliases.ApplicationFrameHost.wndCalculadora.Close();
 }
 
 function operacaoEncadeada() {
   TestedApps.calc.Run();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.Teclado_num_rico.Cinco.Click();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.Operadores_padr_o.Mais.Click();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.Teclado_num_rico.Tr_s.Click();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.Operadores_padr_o.Multiplicar_por.Click();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.Teclado_num_rico.Dois.Click();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.Operadores_padr_o.Igual_a.Click();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.A_exibi_o_0.TextContainer.NormalOutput.Click(30, 30);
-  aqObject.CheckProperty(Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.A_exibi_o_16.TextContainer.NormalOutput, "Text", cmpEqual, "16");
+  Delay(2000);
+  
+  Sys.Keys("5");
+  Sys.Keys("+");
+  Sys.Keys("3");
+  Sys.Keys("*");
+  Sys.Keys("2");
+  Sys.Keys("[Enter]");
+  
+  Delay(500);
+  
+  // Encontra o display com o resultado "16" de forma genérica
+  var displayArea = Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget;
+  var resultDisplay = displayArea.FindChild("Text", "16", 10);
+  
+  aqObject.CheckProperty(resultDisplay, "Text", cmpEqual, "16");
+  
   Aliases.ApplicationFrameHost.wndCalculadora.Close();
 }
 
 function operacaoNumeroDecimal() {
   TestedApps.calc.Run();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.Teclado_num_rico.Cinco.Click();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.Teclado_num_rico.Separador_decimal.Click();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.Teclado_num_rico.Cinco.Click();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.Operadores_padr_o.Mais.Click();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.Teclado_num_rico.Dois.Click();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.Teclado_num_rico.Separador_decimal.Click();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.Teclado_num_rico.Tr_s.Click();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.Operadores_padr_o.Igual_a.Click();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.A_exibi_o_0.TextContainer.NormalOutput.Click(52, 30);
-  aqObject.CheckProperty(Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.A_exibi_o_7_8.TextContainer.NormalOutput, "Text", cmpEqual, "7,8");
+  Delay(2000);
+  
+  Sys.Keys("5");
+  Sys.Keys(",");
+  Sys.Keys("5");
+  Sys.Keys("+");
+  Sys.Keys("2");
+  Sys.Keys(",");
+  Sys.Keys("3");
+  Sys.Keys("[Enter]");
+  
+  Delay(500);
+  
+  // Encontra o display com o resultado "7,8" de forma genérica (considerando separador brasileiro)
+  var displayArea = Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget;
+  var resultDisplay = displayArea.FindChild("Text", "7,8", 10);
+  
+  aqObject.CheckProperty(resultDisplay, "Text", cmpEqual, "7,8");
+  
   Aliases.ApplicationFrameHost.wndCalculadora.Close();
 }
 
 function repetirUltimaOperacaoComIgual() {
   TestedApps.calc.Run();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.Teclado_num_rico.Quatro.Click();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.Operadores_padr_o.Mais.Click();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.Teclado_num_rico.Tr_s.Click();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.Operadores_padr_o.Igual_a.Click();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.Operadores_padr_o.Igual_a.Click();
-  Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.A_exibi_o_0.TextContainer.NormalOutput.Click(33, 37);
-  aqObject.CheckProperty(Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget.A_exibi_o_10.TextContainer.NormalOutput, "Text", cmpEqual, "10");
+  Delay(2000);
+  
+  Sys.Keys("4");
+  Sys.Keys("+");
+  Sys.Keys("3");
+  Sys.Keys("[Enter]");
+  Sys.Keys("[Enter]"); // Repetir a última operação
+  
+  Delay(500);
+  
+  // Encontra o display com o resultado "10" de forma genérica
+  var displayArea = Aliases.Microsoft_WindowsCalculator.Calculadora.NavView.LandmarkTarget;
+  var resultDisplay = displayArea.FindChild("Text", "10", 10);
+  
+  aqObject.CheckProperty(resultDisplay, "Text", cmpEqual, "10");
+  
   Aliases.ApplicationFrameHost.wndCalculadora.Close();
 }
+
 function executarTodosTestes() {
   somar();
   subtrair();
